4260e2e047301a34fe33cb93b848462f1a8b0e16
Sebastian <sortega@moviltrack.com>
2022-07-26 12:14:54 -0500

    cambiando a arreglo

--- test.js
+++ test.js
@@ -1,6 +1,5 @@
 const { performance } = require("perf_hooks");
-let count = 1;
-let time2;
+let count = 0;
 let mediaVariable = [];
 const NUMERO_LECTURAS = 4;
 factorAjuste = 0.9;
@@ -15,27 +14,32 @@ const flowCount = (flow) => {
   let q = 0;
   let dt = 0;
 
-  setInterval((err, value) => {
+  flow.time = setInterval((err, value) => {
     //console.log("Entro a flowWatch", count);
 
-    if (count % 2 === 0) {
-      time2 = performance.now();
-      count++;
-      dt = time2 - flow.time1;
-      f = 1000 / dt;
-      q = f / 5.5;
+    count++;
 
-      console.log(
-        `t2=${time2}\t t1=${flow.time1}\t Δt=${dt}\t f=${f}\tQ=$${q}`
-      );
+    mediaVariable.push(performance.now());
+    if (mediaVariable.length > 0) {
+      const time1 = mediaVariable[mediaVariable.length - 2];
+      const time2 = mediaVariable[mediaVariable.length - 1];
 
-      mediaVariable = [];
-      //}
-    } else {
-      flow.time1 = performance.now();
-      count++;
+      if (count == 100) {
+        count = 0;
+        for (let n = 0; n < 99; n++) {
+          const t1 = mediaVariable[n];
+          const t2 = mediaVariable[n + 1];
+          const dt = t2 - t1;
+          const f = 1000 / dt;
+          console.log(
+            `t2=${t2}\t t1=${t1}\t Δt=${dt}\t f=${f}\tQ=$${0}\tcount=${n}`
+          );
+        }
+
+        clearInterval(flow.time);
+      }
     }
-  }, 18);
+  }, 16);
 };
 
 flowCount(flow);
